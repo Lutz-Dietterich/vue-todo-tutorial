0d3ea005570d414b7a66581b466d281d08366250
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-17 22:54:11 +0100

    init local starage

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="bg-green-400 h-screen m-0 p-10">
-    <div class="bg-gray-600/60 text-white w-1/2 m-auto mt-12 rounded-lg hover:shadow-lg shadow shadow-gray-800">
-        <div class=" text-center py-5 mb-5 border-b-2 border-blacks">
-          <h1 class="text-3xl py-4 flex justify-center gap-3"> <img class="flex justify-end" src="./assets/checklist.png" alt="">Unsere Todo's</h1>
-          <p class="text-xl">Offene Todo's: {{ todos.length }}</p>
+    <article class="bg-gray-600/60 text-white w-1/2 m-auto mt-12 rounded-lg hover:shadow-lg shadow shadow-gray-800">
+        <section class=" text-center py-5 mb-5 border-b-2 border-blacks">
+          <h1 class="text-3xl py-4 flex justify-center gap-3">
+            <img class="flex justify-end" src="./assets/checklist.png" alt="">Todo's
+          </h1>
+          <p class="text-xl" v-if="openTodos.length > 0">
+            Offene Todo's: {{ openTodos.length }}
+          </p>
+          <p class="text-xl" v-else>
+            Alles erledigt! 
+          </p>
           <div class="flex justify-center bg-white w-4/5 mx-auto mt-5 rounded-full">
-            <input type="text" class=" w-4/5 rounded-l-full text-gray-800 py-0.5 px-3" v-model="newTodo"/>
+            <input type="text" required @keyup.enter="addTodo" class=" w-4/5 rounded-l-full text-gray-800 py-0.5 px-3" v-model="newTodo"/>
             <button class="bg-slate-500 w-1/5 rounded-r-full flex justify-center" @click="addTodo"><img class="self-center" src="./assets/add.png" alt=""></button>
           </div>
-        </div>
+        </section>
         <div v-for="(todo, index) in todos" :key="todo.todo">
           <TodoList 
             :todoprop="todo" 
@@ -17,7 +24,7 @@
             @removetodo-index="deleteTodo"
           />
         </div>
-    </div>
+    </article>
   </div>
 </template>
 
@@ -29,24 +36,45 @@ export default {
   data() {
     return {
       newTodo: "",
-      todos: [
-        {todo: "Einkaufen", done: false},
-        {todo: "Sport", done: false},
-        {todo: "Programmieren", done: true},
-        {todo: "Lesen", done: false},
-
-      ],
+      todos: [],
     };
   },
   methods: {
     toggleDone(index) {
       this.todos[index].done = !this.todos[index].done;
+      this.storeTodos();
     },
     deleteTodo(index) {
       this.todos.splice(index, 1);
+      this.storeTodos();
     },
     addTodo() {
+      if (this.newTodo.length > 0) {
       this.todos.push({todo: this.newTodo, done: false});
+      this.storeTodos();
+      this.newTodo = '';
+      } else {
+        alert("Du muss schon was schreiben.");
+      }
+    },
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    }
+  },
+  mounted() {
+    let data = localStorage.getItem("todos");
+    if (data !== "" && data !== null) {
+    this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  },
+  computed: {
+    openTodos() {
+      const openTodos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+      return openTodos;
     },
   },
   components: {
