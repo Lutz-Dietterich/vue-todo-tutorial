8651197e1f9a043a9c407d9408b547d2009554d4
Lutz Dietterich <lutzdietterich@googlemail.com>
2023-12-17 13:32:04 +0100

    install tailwind

---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ message.toUpperCase() }}</h1>
+    <h1>{{ count }}</h1>
+    <button @click="countUp">Hochzählen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      message: "HellO World",
+      count: 0,
+    };
+  },
+  methods: {
+    countUp() {
+      this.count++;
+    },
+  },
+};
+</script>
+
